74dfcbad533ba63ca453d2de3a20b2b119a46c39
Mukhammadjon <93770723+Muhammadjon-is@users.noreply.github.com>
2022-10-14 16:30:03 +0200

    Add files via upload

---
+++ homeWorks/question.js
@@ -0,0 +1,93 @@
+let questions = [
+    {
+      "question": "What company created Kali Linux",
+      "option1": "Offensive Security",
+      "option2": "Canonical",
+      "option3": "Kali Linux,",
+      "option4": "Debian",
+      'answer': "1",
+    },
+  
+    {
+      "question": "What stand for DDOS",
+      "option1": "Denial of Domain Servie",
+      "option2": "Docker Domain Out Service",
+      "option3": "Distributed denial of service",
+      "option4": "Denial Of Service attack",
+      'answer': "3",
+    },
+  
+    {
+      "question": "What is the NMap",
+      "option1": "Password cracker ",
+      "option2": "Network Mapper",
+      "option3": "Wireless attack",
+      "option4": "All of them",
+      'answer': "2",
+    },
+  
+    {
+      "question": "What is aircrack-ng used for",
+      "option1": "Network Mapper",
+      "option2": "to detect weak passwords",
+      "option3": "Wireless weakness",
+      "option4": "to crack WEP and WPA/WPA2-PSK keys",
+      'answer': "4",
+    },
+  
+    {
+      "question": " What are the components of Linux?",
+      "option1": "Hardware",
+      "option2": "Kernel",
+      "option3": "Shell,",
+      "option4": "All of them",
+      'answer': "4",
+    },
+  
+    {
+      "question": " What are the pen-testing methodologies?",
+      "option1": "Black-Box Testing",
+      "option2": "White-Box Testing",
+      "option3": "Gray-Box Testing",
+      "option4": "All of them",
+      'answer': "4",
+    },
+  
+    {
+      "question": "What is John the Ripper used for",
+      "option1": "Pentesting website",
+      "option2": "Password cracking",
+      "option3": "Hacking mobile phone",
+      "option4": "None of them",
+      'answer': "2",
+    },
+    {
+      "question": "What are the main 3 types of hackers?",
+      "option1": "Black hats, white hats and grey hats",
+      "option2": "Red hats, Green hats and Script hats",
+      "option3": "Blue hats, script hats and grey hats",
+      "option4": "none of them",
+      'answer': "1",
+    },
+
+    {
+      "question": "What can't you do with Linux?",
+      "option1": "Run Windows programs",
+      "option2": "Play games ",
+      "option3": "Get viruses ",
+      "option4": " Take some bus resources away from an existing device and assign the existing device alternative bus-resources that it can use instead (yet).",
+      'answer': "4",
+    },
+
+    {
+      "question": "What animal represents Linux?",
+      "option1": "Tux the Penguin",
+      "option2": "Bob the Mandrake ",
+      "option3": "Bill the evil bat of greeds ",
+      "option4": " George the Wombat.",
+      'answer': "1",
+    },
+    
+    
+  ];
+  
